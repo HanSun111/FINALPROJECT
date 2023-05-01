fb34037c9caeb0c60f7dcadb012d9a06cd7c2109
BT_4N2_29 <hsun7920@bths.edu>
2023-05-01 12:29:49 -0400

    design doc

---
+++ src/TrialFrame.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class TrialFrame extends JFrame {
+    TrialFrame(){
+
+        this.setTitle("THIS IS A GUI FOR TRIAL GAME"); // title
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // actually closes the window
+        this.setSize(600, 400); // size of GUI
+        this.setVisible(true); // make it so you can see gui
+        this.setResizable(false); // not resizable anymore
+
+        //ImageIcon icon = new ImageIcon(); create image icon
+        // frame.setIconImage(icon.getImage);
+
+        this.getContentPane().setBackground(new Color(0, 0, 0));
+
+
+        JLabel l = new JLabel();
+        l.setText("New Gui who dis?");
+    }
+}
---
+++ src/Trials.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+
+// for logic and iterating.
+public class Trials {
+
+    public static void main(String[] args) {
+
+        TrialFrame t = new TrialFrame();
+
+    /*    JFrame frame = new JFrame();
+        frame.setTitle("THIS IS A GUI FOR TRIAL GAME"); // title
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // actually closes the window
+        frame.setSize(1800, 1000); // size of GUI
+        frame.setVisible(true); // make it so you can see gui
+        frame.setResizable(false); // not resizable anymore
+
+        //ImageIcon icon = new ImageIcon(); create image icon
+        // frame.setIconImage(icon.getImage);
+
+        frame.getContentPane().setBackground(new Color(0, 0, 0));
+     */
+    }
+}
